390f5f216ad3477df89604cab00cc37311fa95c7
Matt K <matkips@gmail.com>
2018-04-20 15:52:37 -0700

    add listener threads option (#42)
    allow users to configure consumer to create listeners on separate threads.
    
    listeners by default are run from go threads. we're using the blocking receive message method in the sqs java client (with a 10 second poll timeout), so wtih a large number of queues, the listeners block other go loops. squeedo will now instead create listeners in `clojure.core.async/thread`s when the `:listener-threads?` option is et to true.

--- project.clj
+++ project.clj
@@ -10,18 +10,18 @@
 ;; "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 ;; or implied.  See the License for the specific language governing permissions
 ;; and limitations under the License.
-(defproject com.climate/squeedo "1.0.1-SNAPSHOT"
+(defproject com.climate/squeedo "1.1.0-SNAPSHOT"
   :description "Squeedo: The sexiest message consumer ever (™)"
   :url "http://github.com/TheClimateCorporation/squeedo/"
   :min-lein-version "2.0.0"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/core.async "0.3.442"]
-                 [cheshire "5.7.0"]
-                 [com.amazonaws/aws-java-sdk-sqs "1.11.98"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [cheshire "5.8.0"]
+                 [com.amazonaws/aws-java-sdk-sqs "1.11.306"]]
 
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :dependencies [[http-kit "2.2.0"]
@@ -29,12 +29,15 @@
                                    :exclusions [com.amazonaws/aws-java-sdk]]
                                   [com.climate/claypoole "1.1.4"]]}}
 
-  :plugins [[lein-ancient "0.5.5"]]
+  :plugins [[lein-ancient "0.6.15"]]
 
   :deploy-repositories [["releases" :clojars]]
 
-  :test-selectors {:default #(not-any? % #{:integration :benchmark :manual})
+  :test-selectors {:default #(not-any? % #{:integration :benchmark
+                                           :example-cpu :example-listener-threads})
                    :integration :integration
+                   :wip :wip
                    :benchmark :benchmark
-                   :manual :manual
+                   :example-cpu :example-cpu
+                   :example-listener-threads :example-listener-threads
                    :all (fn [_] true)})
